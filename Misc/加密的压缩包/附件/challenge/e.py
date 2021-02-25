ce8a83ac723f6959a568aa5fdcaac8c5570ca052
Jiangli Chai <jianglichai@JianglideMacBook-Pro.local>
2021-02-25 16:29:33 +0800

    上传内存取证&你的名字两题的wp

--- Misc/加密的压缩包/附件/challenge/e.py
+++
@@ -1,8 +0,0 @@
-import base64
-flag="xgctf{**************}"
-flag_base64=base64.b64encode(flag.encode())
-print(flag_base64)
-for i in range(0,len(flag_base64),2):
-    f=open("data{}.txt".format(str(i)),"wb")
-    f.write(flag_base64[i:i+2])
-    f.close()
